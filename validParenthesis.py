5ae8d4e4453ac55250118f0873a56b5d970f866d
Sameer Bandha <147968242+Sameer-B21@users.noreply.github.com>
2025-11-19 03:34:49 -0500

    easy

---
+++ validParenthesis.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        dic = {'(':')', '{':'}', '[':']'}
+        op = dic.keys()
+        stack = []
+        for i in s:
+            if i in op:
+                stack.append(i)
+            elif len(stack) == 0 and i not in op:
+                return False
+            elif i != dic[stack.pop()]:
+                return False
+        if len(stack) != 0:
+            return False
+        return True
